253254e6bb762061a9994b22d835364df042e35c
Wen.Liao <zzz00072@gmail.com>
2014-04-01 16:04:32 +0800

    Run gdb automatically. Referenced from southernbear/rtenv
    Reference:
    https://github.com/southernbear/rtenv/blob/master/mk/qemu.mk

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -5,7 +5,11 @@ qemu: $(OUT_DIR)/$(TARGET).bin $(QEMU_STM32)
 qemudbg: $(OUT_DIR)/$(TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-gdb tcp::3333 -S \
-		-kernel $(OUT_DIR)/$(TARGET).bin -monitor null
+		-kernel $(OUT_DIR)/$(TARGET).bin -monitor null & \
+		echo $$! > $(OUT_DIR)/qemu_pid && \
+		$(CROSS_COMPILE)gdb -x $(TOOLS_DIR)/gdbscript && \
+		cat $(OUT_DIR)/qemu_pid | `xargs kill 2>/dev/null || test true` && \
+		rm -f $(OU_TDIR)/qemu_pid
 
 qemu_remote: $(OUT_DIR)/$(TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 -kernel $(OUT_DIR)/$(TARGET).bin -vnc :1
